28fbaf08d1f1a4452daf1ab0631c8c41fa76b36c
Marco <10533178@polimi.it>
2021-07-19 11:33:35 +0200

    charset transcoding check added

--- toolnicode/Toolnicode.py
+++ toolnicode/Toolnicode.py
@@ -12,10 +12,15 @@ URL = 'http://localhost:5000/'
 NAME = 'content'
 PATH_CHROME = "Browser_Selenium/chromedriver.exe"
 TOKEN_LENGTH = 8
+
+MULTIPLICATION_SIGN = '\u00d7'
+REPLACEMENT_CHARACTER= '\ufffd'
 S_POINTED = "\u1e69"
 FILEGATURE = "\ufb01"
 A_UPPER = "\u0041"
 A_LOWER = "\u0061"
+GREEK_QUESTION_MARK = "\u037e"
+SEMICOLON = "\u003b"
 GREEK_NUMERAL_SIGN = '\u0374'
 MODIFIER_LETTER_PRIME = '\u02b9' #char obtained from GREEK_NUMERAL_SIGN normalization of each type
 #Escaping
@@ -29,6 +34,7 @@ escaping_dict = {
     "APOSTROPHE" : '\u0027',
     "SOLIDUS" : '\u002f',
     "REVERSE_SOLIDUS" : '\u005c',
+    "SEMICOLON" : '\u003b',
     "LESSTHAN_SIGN" : '\u003c',
     "GREATERTHAN_SIGN" : '\u003e',
     "QUESTION_MARK" : '\u003f',
@@ -67,6 +73,7 @@ def injection(char):
   char = re.search(token + ".*" + token, x.text, re.IGNORECASE)
   if char:
     char = char.group()[TOKEN_LENGTH:-TOKEN_LENGTH]
+    #print(hex(ord(char)))
     return char
   else:
     return ''
@@ -86,17 +93,29 @@ def compatibilityParser(payload):
   payload = payload.replace("?", "\uff1f")
   return payload
 
-def normalizationCheck ():
+def normalizationCheck (ascii):
 #Normalization
-  char = injection(GREEK_NUMERAL_SIGN)
-  if char == MODIFIER_LETTER_PRIME:
-    pass
-  elif char == GREEK_NUMERAL_SIGN: 
-    greenPrint("There is NO normalization")
-    return
-  else:
-    yellowPrint("something goes wrong")
-    return
+  if ascii == 1:
+    char = injection(GREEK_NUMERAL_SIGN)
+    if char == MODIFIER_LETTER_PRIME:
+      pass
+    elif char == GREEK_NUMERAL_SIGN: 
+      greenPrint("There is NO normalization")
+      return
+    else:
+      normalizationCheck(2)
+      #yellowPrint("something goes wrong")
+      return
+  if ascii == 2:
+    char = injection(GREEK_QUESTION_MARK)
+    if char == SEMICOLON:
+      pass
+    elif char == GREEK_QUESTION_MARK: 
+      greenPrint("There is NO normalization")
+      return
+    else:
+      yellowPrint("something goes wrong")
+      return 
   #Form
   char = injection(S_POINTED)
   if(len(char) == 3):
@@ -144,6 +163,24 @@ def escapingCheck():
     else:
       greenPrint(key + " (" + escaping_dict[key] + ")" + " has not been escaped.")
 
+def charsetTranscodingCheck():
+  char = injection(GREEK_NUMERAL_SIGN)
+  if char == GREEK_NUMERAL_SIGN: 
+    greenPrint("UTF-8 encoding supported")
+    return
+  elif char == '' or '?' in char or REPLACEMENT_CHARACTER in char:
+    redPrint("UTF-8 encoding NOT supported")
+  else:
+    yellowPrint("Something goes wrong!")
+    return
+  char = injection(MULTIPLICATION_SIGN)
+  if char == MULTIPLICATION_SIGN: 
+    greenPrint("Latin-1 encoding supported")
+  else:
+    redPrint("Latin-1 encoding NOT supported")
+    greenPrint("ASCII encoding supported")
+    return
+  
 def injectNormalizationPayload(dimension):
   if dimension == "1":
     file = "xss_payload_10.txt"
@@ -183,6 +220,7 @@ def injectNormalizationPayload(dimension):
 def main():
   #for colored terminal
   init(convert=True)
+  '''
   parser = argparse.ArgumentParser()
   required = parser.add_argument_group('required arguments')
   required.add_argument("-u", "--url", help = "url to scan", required=True)
@@ -194,14 +232,15 @@ def main():
   if args.name:
     global NAME
     NAME = args.name
+    '''
   while(True):
-    print("Choose which analisys perform:\n[1] Normalization\n[2] Casing\n[3] Escaping\n[0] Quit\n")
+    print("Choose which analisys perform:\n[1] Normalization\n[2] Casing\n[3] Escaping\n[4] Charset transcoding (valid only if there are no normalization)\n[0] Quit\n")
     choose = input()
     if choose == "1":
       print("You want perform an analysis or an injection?\n[1] Scan for unicode normalization\n[2] Try xss injection\n")
       sub_choose = input()
       if sub_choose == "1":
-        normalizationCheck()
+        normalizationCheck(1)
       elif sub_choose == "2":
         print("Choose payloads:\n[1] load 10 payloads\n[2] load 500 payloads\n[3] load 6000+ payloads\n")
         sub_choose = input()
@@ -216,7 +255,9 @@ def main():
     elif choose == "3":
       escapingCheck()
     elif choose == "4":
-      injectNormalizationPayload()
+      charsetTranscodingCheck()
+    elif choose == "5":
+      pass
     elif choose == "0":
       return
     else:
